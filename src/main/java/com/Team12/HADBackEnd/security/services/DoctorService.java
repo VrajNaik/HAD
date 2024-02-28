8731ffa566ff1cf6115867a5534db2e4bd1d1f3e
VrajNaik <vrajnaik5421@gmail.com>
2024-02-28 17:08:43 +0530

    Custome Password Hashing Done

--- src/main/java/com/Team12/HADBackEnd/security/services/DoctorService.java
+++ src/main/java/com/Team12/HADBackEnd/security/services/DoctorService.java
@@ -117,7 +117,7 @@ public class DoctorService {
         }
 
         // Generate a unique username
-//        String generatedUsername = generateUniqueUsername();
+        // String generatedUsername = generateUniqueUsername();
         doctor.setUsername(generatedUsername);
         doctor.setAge(doctor.getAge());
         doctor.setDistrict(doctor.getDistrict());
