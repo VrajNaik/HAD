2168d1fcf90e190f055438cc4469367cec1f9e50
VrajNaik <vrajnaik5421@gmail.com>
2024-02-28 17:09:14 +0530

    Random Username generation

--- src/main/java/com/Team12/HADBackEnd/security/services/DoctorService.java
+++ src/main/java/com/Team12/HADBackEnd/security/services/DoctorService.java
@@ -118,6 +118,7 @@ public class DoctorService {
 
         // Generate a unique username
         // String generatedUsername = generateUniqueUsername();
+
         doctor.setUsername(generatedUsername);
         doctor.setAge(doctor.getAge());
         doctor.setDistrict(doctor.getDistrict());
