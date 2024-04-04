80c82dfe35969da859e4b406db9a3e46bb72fd8b
ARJUN1220 <arjungangani12@gmail.com>
2024-04-04 13:04:03 +0530

    added twilio service

---
+++ src/main/java/com/Team12/HADBackEnd/utils/Constant.java
@@ -0,0 +1,9 @@
+package com.Team12.HADBackEnd.utils;
+
+public interface Constant {
+
+    //----------------------Twilio Credentials----------------------------//
+    String ACCOUNT_SID = "AC0518276df4ce7808b4641885bf208809";
+    String AUTH_TOKEN = "4ecc105f8e0e657093ab41e15da1a15b";
+    //--------------------------------------------------------------------//
+}
