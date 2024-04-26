c39a10d389a732beaf04f6cc0139234b0b1e4469
ARJUN1220 <55222444+ARJUN1220@users.noreply.github.com>
2024-04-26 17:54:49 +0530

    Update Constant.java

--- src/main/java/com/Team12/HADBackEnd/util/Constant.java
+++ src/main/java/com/Team12/HADBackEnd/util/Constant.java
@@ -4,7 +4,7 @@ public interface Constant {
 
     //----------------------Twilio Credentials----------------------------//
     String ACCOUNT_SID = "AC0518276df4ce7808b4641885bf208809";
-    String AUTH_TOKEN = "";
+    String AUTH_TOKEN = "60038aaa23541d0d53ea6ce3a336b11c";
     //--------------------------------------------------------------------//
 
     //---------------------Numbers-----------------------------------//
