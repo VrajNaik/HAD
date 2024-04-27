626a65c5cf773381d33b537d69133dd2473994e8
ARJUN1220 <arjungangani12@gmail.com>
2024-04-27 12:54:34 +0530

    changes

--- src/main/java/com/Team12/HADBackEnd/util/Constant.java
+++ src/main/java/com/Team12/HADBackEnd/util/Constant.java
@@ -1,9 +1,28 @@
 package com.Team12.HADBackEnd.util;
 
+import java.util.Random;
+
 public interface Constant {
+
+    //---------------------------Roles---------------------------------//
+    String FIELD_HEALTH_WORKER = "FHW";
+    //-----------------------------------------------------------------//
 
     //----------------------Twilio Credentials----------------------------//
     String ACCOUNT_SID = "AC0518276df4ce7808b4641885bf208809";
     String AUTH_TOKEN = "8235aa6629a7ce0c5cd75bf58e2193c6";
     //--------------------------------------------------------------------//
+
+    //---------------------Numbers-----------------------------------//
+    Integer OTP_LENGTH = 6;
+    //---------------------------------------------------------------//
+
+    // Generating OTPs
+    static String generateOtp() {
+        String numbers = "0123456789";
+        String otp = "";
+        for(int i = 0; i < OTP_LENGTH; i++)
+            otp += numbers.charAt(new Random().nextInt(numbers.length()));
+        return otp;
+    }
 }
